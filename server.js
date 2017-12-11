24416638a95f84767bdfd070774f1c6dfc9eb21b
alanunruh <alan.unruh@gmail.com>
2017-12-11 15:55:29 -0500

    Setting up the index view

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
+app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render('index');
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Weather App listening on port 3000!')
 })
