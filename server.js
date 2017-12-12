4ee2f45cf45ce7bb3800ff54d3d782acf8b2cea9
alanunruh <alan.unruh@gmail.com>
2017-12-12 08:40:05 -0500

    Change order of calls

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 
-app.set('view engine', 'ejs')
 app.use(express.static('public'));
+app.set('view engine', 'ejs')
+
 
 app.get('/', function (req, res) {
   res.render('index');
