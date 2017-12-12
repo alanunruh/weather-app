646e883681db6745f7f4ef4bae99f4957e00979e
alanunruh <alan.unruh@gmail.com>
2017-12-12 15:47:46 -0500

    Make API call and display weather

--- server.js
+++ server.js
@@ -14,9 +14,22 @@ app.get('/', function (req, res) {
 
 app.post('/', function (req, res) {
   let city = req.body.city;
-  let url = 'http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&apid=${apiKey}'
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
 })
 
 app.listen(3000, function () {
   console.log('Weather App listening on port 3000!')
-})
+});
